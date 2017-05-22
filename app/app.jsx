1288db2e2a3dcf16c328740d36e3d88bb39516fe
prog4life <prog4life@users.noreply.github.com>
2017-05-23 02:53:23 +0300

    Support Foundation settings

--- app/app.jsx
+++ app/app.jsx
@@ -6,16 +6,15 @@ var Timer = require('Timer');
 var Countdown = require('Countdown');
 
 // Load foundation
-require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
-// App css:
+// App css
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
-      <Route path="/countdown" component={Countdown}/>
+      <Route path="countdown" component={Countdown}/>
       <IndexRoute component={Timer}/>
     </Route>
   </Router>,
